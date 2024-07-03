4097cd70e84dc21d2a01f935ee7f98ef88b257d3
neeraj5696 <neerajkumar5696@gmail.com>
2024-07-03 18:23:23 +0530

    hello

--- Module_2/callback.js
+++ Module_2/callback.js
@@ -1,15 +1,15 @@
-let operations =  ["5","-2","4","C","9","D"]
+let operations =  ["5","-2","4","C","9","D", "+"]
 
 var callPoints = function(operations) {
-    let emp = [];
+    let emp = [], sum=0
     for (let i = 0; i < operations.length; i++) {
         if (operations[i] === "+") {
-         let   sum = parseInt(operations[i - 2]) + parseInt(operations[i - 1]);
-            emp[i] = sum;
+            sum = parseInt(emp[emp.length-1]) + parseInt(emp[emp.length-2]);
+            emp.push(sum);
         }
          else if (operations[i] === "D") {
           let  newD = parseInt(operations[i - 1]) * 2;
-            emp[i] = newD;
+            emp.push(newD);
         }
         else if (operations[i] === "C") {
             emp.pop();
